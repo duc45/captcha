23c9ca51fc73aba9c94ec33158661df9cf28fa4b
duc45 <phanminhduc45@gmail.com>
2018-09-19 18:05:54 +0700

    last updated

--- test_keras_CNN_RNN.py
+++ test_keras_CNN_RNN.py
@@ -16,7 +16,7 @@ img_width, img_height = 150, 50
 train_data_dir = "/home/duc/Downloads/captcha/captcha1/train"
 test_data_dir = "/home/duc/Downloads/captcha/captcha1/test"
 epochs = 10
-list_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+list_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
 list_chars_len = len(list_chars)
 
 def get_label(img):
@@ -43,54 +43,53 @@ test_image = data(test_data_dir)
 
 train_img_data = np.array([i[0] for i in train_image])
 train_img_label = np.array([i[1] for i in train_image])
-train_img_label.shape = (-1,372)
-print(train_img_data.shape)
-print(train_img_label.shape)
+#train_img_label.shape = (-1,372)
 
 
 test_img_data = np.array([i[0] for i in test_image])
 test_img_label = np.array([i[1] for i in test_image])
-test_img_label.shape = (-1,372)
-print(test_img_data.shape)
-print(test_img_label.shape)
+#test_img_label.shape = (-1,372)
 
 
 		# BUILD MODEL
+cnn_act = 'relu'
 
 cnn = Sequential()
 # Input 150x50 color image, using 32 filter size 3x3
 
-cnn.add(Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=(img_height,img_width,3)))
+cnn.add(Conv2D(64, (3, 3), activation=cnn_act, padding='same', input_shape=(img_height,img_width,3)))
 cnn.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 cnn.add(Dropout(0.25))
 
-cnn.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+cnn.add(Conv2D(128, (3, 3), activation=cnn_act, padding='same'))
 cnn.add(MaxPooling2D(pool_size=(2,2), strides=2))
-cnn.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-cnn.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+cnn.add(Conv2D(256, (3, 3), activation=cnn_act, padding='same'))
+cnn.add(Conv2D(256, (3, 3), activation=cnn_act, padding='same'))
 cnn.add(MaxPooling2D(pool_size=(1,2), strides=2))
 cnn.add(BatchNormalization(axis=1))
-cnn.add(Dropout(0.25))
-cnn.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-cnn.add(BatchNormalization(axis=1))
-cnn.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-cnn.add(MaxPooling2D(pool_size=(1,2), strides=2))
-cnn.add(Conv2D(512, (2, 2), activation='relu'))
 cnn.add(Dropout(0.25))
 
+cnn.add(Conv2D(512, (3, 3), activation=cnn_act, padding='same'))
+cnn.add(BatchNormalization(axis=1))
+cnn.add(Conv2D(512, (3, 3), activation=cnn_act, padding='same'))
+cnn.add(MaxPooling2D(pool_size=(1,2), strides=2))
+#cnn.add(Conv2D(512, (3, 3), activation=cnn_act))
+#cnn.add(Conv2D(512,(1,2), activation=cnn_act))
+cnn.add(Dropout(0.25))
+print(cnn.output_shape)
+
 model = Sequential()
-model.add(TimeDistributed(cnn, input_shape=(img_height,img_width,3)))
-model.add(Reshape((16,512)))
+model.add(cnn)
+model.add(Reshape((6,2304)))
 model.add(Bidirectional(LSTM(256, return_sequences=True)))
 model.add(Dropout(0.25))
 model.add(Bidirectional(LSTM(256, return_sequences=True)))
-model.add(Flatten())
-#model.add(Reshape(10374,372))
-model.add(Dense(372, activation='softmax'))
-
+model.add(TimeDistributed(Dense(63, activation='softmax')))
+#model.add(Flatten())
+#model.add(Dense(372, activation='softmax'))
 #ada_op = Adagrad(lr=0.015)
 model.compile(loss='categorical_crossentropy',
-				optimizer='rmsprop',
+				optimizer='adadelta',
 				metrics=['accuracy'])
 
 model.fit(train_img_data, train_img_label, batch_size=64, 
