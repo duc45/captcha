59c9e3eb7fbc6a54d3cea27fd16c3939b7895853
duc45 <phanminhduc45@gmail.com>
2018-10-15 11:18:49 +0700

    update

--- test_CTC.py
+++ test_CTC.py
@@ -1,19 +1,13 @@
 import numpy as np
-<<<<<<< HEAD
 import editdistance
 import datetime
 import matplotlib.pyplot as plt
-=======
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
 import keras
 import os
 import re
 import cv2
 import itertools
-<<<<<<< HEAD
 import keras.callbacks
-=======
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
 from random import shuffle
 from tqdm import tqdm
 from keras import backend as K
@@ -28,21 +22,15 @@ from keras.optimizers import *
 
 train_data_dir = "/home/duc/Downloads/captcha/captcha1/train"
 test_data_dir = "/home/duc/Downloads/captcha/captcha1/test"
-<<<<<<< HEAD
 val_data_dir = "/home/duc/Downloads/captcha/captcha1/validation"
-=======
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
+
 out_put_dir = "test_captcha_model"
 epochs = 10
 regex = r'^[a-z A-Z]+$'
 list_chars = u'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz '
 list_chars_len = len(list_chars)
-<<<<<<< HEAD
 
-=======
 max_str_len = 16
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
-
 img_width, img_height = 100, 32
 
 def get_label(img):
@@ -60,13 +48,11 @@ def get_text(label):
       text.append(list_chars[c])
   return "".join(text)
 
-<<<<<<< HEAD
-=======
 def is_valid_str(str):
   search = re.compile(regex, re.UNICODE).search
   return bool((search(str)))
 
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
+
 def data(data_dir):
   data = []
   for i in tqdm(os.listdir(data_dir)):
@@ -74,7 +60,6 @@ def data(data_dir):
     temp = cv2.imread(path, cv2.IMREAD_COLOR)
     img = cv2.resize(temp, (img_width,img_height))
     label = get_label(i.split(".")[0])
-<<<<<<< HEAD
     data.append([np.array(img),label,len(i.split(".")[0])])
   shuffle(data)
   return data
@@ -100,10 +85,9 @@ test_img_label = np.array([i[1] for i in test_image])
 '''
 
 		#CTC lambda function
-=======
+
 
 #CTC lambda function
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
 def ctc_lambda_func(args):
   y_pred, labels, input_length, label_length = args
   y_pred = y_pred[:, 2:, :]
@@ -119,7 +103,6 @@ def decode_batch(test_func, word_batch):
     ret.append(out_str)
   return ret
 
-<<<<<<< HEAD
 #generator class 
 class DataGenerator(keras.callbacks.Callback):
 	def __init__(self, train_dir,
@@ -176,9 +159,9 @@ class DataGenerator(keras.callbacks.Callback):
 		for i in range(size):
 			if train:
 				if K.image_data_format() == 'channels_first':
-					X_data[i, :, 0:self.im_w, :] = self.train_X[index + i]
+					X_data[i, :, :, :] = self.train_X[index + i]
 				else:
-					X_data[i, 0:self.im_w, :, :] = self.train_X[index + i]
+					X_data[i, :, :, :] = self.train_X[index + i]
 				labels[i, :] = self.train_Y[index + i]
 				input_length[i] = self.im_w // self.ds_factor - 2
 				label_length[i] = self.train_Y_len[index + i]
@@ -266,7 +249,7 @@ def on_epoch_end(self, epoch, logs={}):
 	fig.set_size_inches(10, 13)
 	plt.savefig(os.path.join(self.output_dir, 'e%02d.png' % (epoch)))
 	plt.close()
-=======
+
 # Call back class for visualize training process
 '''class Visualize_callback(keras.callback.Callback):
 
@@ -290,13 +273,13 @@ def on_epoch_end(self, epoch, logs={}):
 
 
 
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
+
 
 
     # BUILD MODEL
 
 #setup parameter
-<<<<<<< HEAD
+
 def train(run_name, start_epoch, stop_epoch):
 	cnn_act = 'relu'
 	cnn_kernel = (3,3)
@@ -398,80 +381,3 @@ def train(run_name, start_epoch, stop_epoch):
 run_name = datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
 train(run_name, 0, 20)
 
-=======
-cnn_act = 'relu'
-cnn_kernel = (3,3)
-
-rnn_size = 512
-kernel_init = 'he_normal'
-
-if K.image_data_format() == 'channels_first':
-  input_shape = (3, img_width, img_height)
-else:
-  input_shape = (img_width, img_height, 3)
-
-# Input 150x50 color image, using 32 filter size 3x3
-Input_data = Input(name='the_input', shape=input_shape, dtype='float32')
-cnn = Conv2D(64, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv1')(Input_data)
-cnn = MaxPooling2D(pool_size=(2, 2), strides=2, name='max1')(cnn)
-cnn = Dropout(0.25)(cnn)
-
-cnn = Conv2D(128, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv2')(cnn)
-cnn = MaxPooling2D(pool_size=(2,2), strides=2)(cnn)
-cnn = Conv2D(256, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv3')(cnn)
-cnn = Conv2D(256, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv4')(cnn)
-cnn = MaxPooling2D(pool_size=(1,2), strides=2)(cnn)
-cnn = Dropout(0.25)(cnn)
-cnn = Conv2D(512, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv5')(cnn)
-cnn = BatchNormalization(axis=1)(cnn)
-cnn = Conv2D(512, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv6')(cnn)
-cnn = BatchNormalization(axis=1)(cnn)
-cnn = MaxPooling2D(pool_size=(1,2), strides=2)(cnn)
-cnn = Conv2D(512, cnn_kernel, activation=cnn_act, padding='same',
-                kernel_initializer=kernel_init, name='conv7')(cnn)
-#cnn.add(Conv2D(512,(1,2), activation=cnn_act))
-cnn = Dropout(0.25)(cnn)
-
-conv_to_rnn_dims = (img_width//(1**2), (img_height // (2**2))*512)
-#print(conv_to_rnn_dims)
-#print(cnn.shape)
-#rnn = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(cnn)
-rnn = Dense(32, activation=cnn_act, name='dense1')(cnn)
-rnn = Reshape(target_shape=(14,32))(rnn)
-rnn = Bidirectional(LSTM(rnn_size, return_sequences=True,
-          kernel_initializer=kernel_init, name='lstm1'))(rnn)
-rnn = Dropout(0.25)(rnn)
-rnn = Bidirectional(LSTM(rnn_size, return_sequences=True,
-          kernel_initializer=kernel_init, name='lstm2'))(rnn)
-rnn = Dense(list_chars_len+1, kernel_initializer=kernel_init
-          ,name='dense2')(rnn)
-y_pred = Activation('softmax', name='softmax')(rnn)
-#Model(inputs=Input_data, outputs=y_pred).summary()
-
-labels = Input(name='the_labels', shape=[max_str_len], dtype='float32')
-input_length = Input(name='input_length', shape=[1], dtype='int64')
-label_length = Input(name='label_length', shape=[1], dtype='int64')
-
-loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
-
-sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
-
-model = Model(inputs=[Input_data, labels, input_length, label_length], outputs=loss_out)
-model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
-
-epochs = 10
-model.fit(train_img_data, train_img_label, batch_size=32, 
-          epochs=epochs, verbose=1, validation_data=(test_img_data,test_img_label))
-
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-  json_file.write(model_json)
-model.save_weights("model.h5")
-print("Saved model to disk")
->>>>>>> 228f807f0087345963fbd4e63c0501d445c9a168
