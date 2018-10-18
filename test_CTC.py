4e79e9d3cfd9d681e245b9dc01ff643654828863
duc45 <phanminhduc45@gmail.com>
2018-10-18 08:07:05 +0700

    update

--- test_CTC.py
+++ test_CTC.py
@@ -8,6 +8,7 @@ import re
 import cv2
 import itertools
 import keras.callbacks
+import pylab
 from random import shuffle
 from tqdm import tqdm
 from keras import backend as K
@@ -24,7 +25,7 @@ train_data_dir = "/home/duc/Downloads/captcha/captcha1/train"
 test_data_dir = "/home/duc/Downloads/captcha/captcha1/test"
 val_data_dir = "/home/duc/Downloads/captcha/captcha1/validation"
 
-out_put_dir = "test_captcha_model"
+out_put_dirs = "test_captcha_model"
 epochs = 10
 regex = r'^[a-z A-Z]+$'
 list_chars = u'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz '
@@ -37,6 +38,8 @@ def get_label(img):
   label = []
   for c in img:
     label.append(list_chars.find(c))
+  for c in range(max_str_len - len(label)):
+  	label.append(list_chars_len)
   return label
 
 def get_text(label):
@@ -106,13 +109,15 @@ def decode_batch(test_func, word_batch):
 #generator class 
 class DataGenerator(keras.callbacks.Callback):
 	def __init__(self, train_dir,
-		im_h, im_w, ds_factor,
-		val_dir, batch_size, 
+		im_h, im_w, ds_factor,train_size,
+		val_dir, batch_size, val_size,
 		n_channels=3, shuffle=True, max_str_leng=16):
 		
 		self.train_dir = train_dir
 		self.val_dir = val_dir
 		self.batch_size= batch_size
+		self.train_size = train_size
+		self.val_size =val_size
 		self.n_channels = n_channels
 		self.shuffle = shuffle
 		self.max_str_leng = max_str_leng
@@ -127,54 +132,67 @@ class DataGenerator(keras.callbacks.Callback):
 
 	def get_data(self):
 		train_data = data(self.train_dir)
+		train_X =[]
+		train_Y = []
+		train_Y_len = []
 		for i in train_data:
-			self.train_X = i[0]
-			self.train_Y = i[1]
-			self.train_Y_len = i[2]
-		
-		val_data = data(self.val_dir)
-		for i in val_data:
-			self.val_X = i[0] 
-			self.val_Y = i[1]
-			self.val_Y_len = i[2]
-		
-		if K.image_data_format() == 'channels_first':
-			self.train_X.shape = (3, self.im_w, self.im_h)
-			self.val_X.shape = (3, self.im_w, self.im_h)
-		else:
-			self.train_X.shape = (self.im_w, self.im_h, 3)
-			self.val_X.shape = (self.im_w, self.im_h, 3)
+			train_X.append(i[0])
+			train_Y.append(i[1])
+			train_Y_len.append(i[2])
+		self.train_X = np.array(train_X)
+		self.train_Y = np.array(train_Y)
+		self.train_Y_len = np.array(train_Y_len)
 
+		#self.train_X = np.array(i[0] for i in train_data)
+		#self.train_Y = np.array(i[1] for i in train_data)
+		#self.train_Y_len = np.array(i[2]for i in train_data)
+
+		val_data = data(self.val_dir)
+		val_X = []
+		val_Y = []
+		val_Y_len = []
+		for i in val_data:
+			val_X.append(i[0])
+			val_Y.append(i[1])
+			val_Y_len.append(i[2])
+		self.val_X = np.array(val_X)
+		self.val_Y = np.array(val_Y)
+		self.val_Y_len = np.array(val_Y_len)
+
+		#self.val_X = np.array(i[0] for i in val_data)
+		#self.val_Y = np.array(i[1] for i in val_data)
+		#self.val_Y_len = np.array(i[2] for i in val_data)
+
+		'''
+		if K.image_data_format() == 'channels_first':
+			self.train_X.shape = (self.train_size, 3, self.im_w, self.im_h)
+			self.val_X.shape = (self.val_size, 3, self.im_w, self.im_h)
+		else:
+			self.train_X.shape = (self.train_size, self.im_w, self.im_h, 3)
+			self.val_X.shape = (self.val_size, self.im_w, self.im_h, 3)
+			'''
 
 	def get_batch(self, index, size, train):
-		if K.image_data_format() == 'channels_first':
-			X_data = np.ones([size, 3, img_width, img_height])
-		else:
-			X_data = np.ones([size, img_width, img_height, 3])
-
-		labels = np.ones([size, self.max_str_leng])
+		X_data = []
+		labels = []
 		input_length = np.ones([size, 1])
 		label_length = np.ones([size, 1])
 		source_str = []
 		for i in range(size):
 			if train:
-				if K.image_data_format() == 'channels_first':
-					X_data[i, :, :, :] = self.train_X[index + i]
-				else:
-					X_data[i, :, :, :] = self.train_X[index + i]
-				labels[i, :] = self.train_Y[index + i]
+				X_data.append(self.train_X[index + i])
+				labels.append(self.train_Y[index + i])
 				input_length[i] = self.im_w // self.ds_factor - 2
 				label_length[i] = self.train_Y_len[index + i]
 				source_str.append(get_text(labels[i]))
 			else:
-				if K.image_data_format() == 'channels_first':
-					X_data[i, :, 0:self.im_w, :] = self.val_X[index + i]
-				else:
-					X_data[i, 0:self.im_w, :, :] = self.val_X[index + i]
-				labels[i, :] = self.val_Y[index + i]
+				X_data.append(self.val_X[index + i])
+				labels.append(self.val_Y[index + i])
 				input_length[i] = self.im_w // self.ds_factor - 2
 				label_length[i] = self.val_Y_len[index + i]
 				source_str.append(get_text(labels[i]))
+		X_data = np.array(X_data)
+		labels = np.array(labels)
 		inputs = {
 			'the_input': X_data,
 			'the_labels': labels,
@@ -187,25 +205,30 @@ class DataGenerator(keras.callbacks.Callback):
 
 	def next_train(self):
 		while 1:
-			ret = self.get_batch(self.cur_train_index, self.batch_size, train = True)
+			self.cur_train_index = 0
+			self.cur_val_index = 0
+			ret = self.get_batch(0, self.batch_size, train = True)
 			self.cur_train_index += self.batch_size
 			yield ret
 
 	def next_val(self):
 		while 1:
-			ret = self.get_batch(self.cur_val_index, self.batch_size, train=False)
+			self.cur_train_index = 0
+			self.cur_val_index = 0
+			ret = self.get_batch(0, self.batch_size, train=False)
 			self.cur_val_index += self.batch_size
 			yield ret
 
 	def on_train_begin(self, logs={}):
 		self.get_data()
 
+
 # Call back class for visualize training process
 class Visualize_callback(keras.callbacks.Callback):
 
 	def __init__(self, run_name, test_func, data_gen, num_display_words=6):
 		self.test_func = test_func
-		self.output_dir = os.path.join(out_put_dir, run_name)
+		self.output_dir = os.path.join(out_put_dirs, run_name)
 		self.data_gen = data_gen
 		self.num_display_words = num_display_words
 		if not os.path.exists(self.output_dir):
@@ -228,46 +251,29 @@ class Visualize_callback(keras.callbacks.Callback):
 		mean_ed = mean_ed / num
 		print('\nOut of %d samples: Mean edit distance: %0.3f Mean normalized edit distance: %0.3f' % (num, mean_ed, mean_norm_ed))
 
-def on_epoch_end(self, epoch, logs={}):
-	self.model.save_weights(os.path.join(self.output_dir, 'weights%0.2d.h5' % (epoch)))
-	self.show_edit_distance(256)
-	word_batch = next(self.data_gen)[0]
-	res = decode_batch(self.test_func, word_batch['the_input'][0:self.num_display_words])
-	if word_batch['the_input'][0].shape[0] < 256:
-		cols = 2
-	else:
-		cols = 1
-	for i in range(self.num_display_words):
-		plt.subplot(self.num_display_words // cols, cols, i+1)
-		if K.image_data_format() == 'channels_first':
-			the_input = word_batch['the_input'][i, 0, :, :]
+	def on_epoch_end(self, epoch, logs={}):
+		self.model.save_weights(os.path.join(self.output_dir, 'weights%0.2d.h5' % (epoch)))
+		self.show_edit_distance(256)
+		word_batch = next(self.data_gen)[0]
+		res = decode_batch(self.test_func, word_batch['the_input'][0:self.num_display_words])
+		if word_batch['the_input'][0].shape[0] < 256:
+			cols = 2
 		else:
-			the_input = word_batch['the_input'][i, :, :, 0]
-		plt.imshow(the_input, cmap='rainbow')
-		plt.xlabel('Truth = \'%s\'\nDecoded = \'%s\'' % (word_batch['source_str'][i], res[i]))
-	fig = pylab.gcf()
-	fig.set_size_inches(10, 13)
-	plt.savefig(os.path.join(self.output_dir, 'e%02d.png' % (epoch)))
-	plt.close()
+			cols = 1
+		for i in range(self.num_display_words):
+			plt.subplot(self.num_display_words // cols, cols, i+1)
+			if K.image_data_format() == 'channels_first':
+				the_input = word_batch['the_input'][i, 0, :, :]
+			else:
+				the_input = word_batch['the_input'][i, :, :, 0]
+			plt.imshow(the_input, cmap='Greys_r')
+			plt.xlabel('Truth = \'%s\'\nDecoded = \'%s\'' % (word_batch['source_str'][i], res[i]))
+		fig = pylab.gcf()
+		fig.set_size_inches(10, 13)
+		plt.savefig(os.path.join(self.output_dir, 'e%02d.png' % (epoch)))
+		plt.close()
 
-# Call back class for visualize training process
-'''class Visualize_callback(keras.callback.Callback):
 
-  def __init__(self, run_name, test_func, text_img_gen, num_display_words=6):
-    self.test_func = test_func
-    self.output_dir = os.path.join(out_put_dir, run_name)
-    self.text_img_gen = text_img_gen
-    self.num_display_words = num_display_words
-    if not os.path.exists(self.output_dir):
-      os.makedirs(self.output_dir)
-   
-  def show_edit_distance(self, num):
-    num_left = num
-    mean_norm_ed = 0.0
-    mean_ed = 0.0
-    while num_left > 0:
-      word_batch = next(self.text_img_gen)[0]
-'''
 
 
 
@@ -286,19 +292,21 @@ def train(run_name, start_epoch, stop_epoch):
 
 	rnn_size = 512
 	kernel_init = 'he_normal'
-	batch_size = 32
+	batch_sizes = 32
 	nb_train = os.listdir(train_data_dir)
 	nb_val = os.listdir(val_data_dir)
 
 	if K.image_data_format() == 'channels_first':
-  		input_shape = (3, img_width, img_height)
+  		input_shape = (3, img_height, img_width)
 	else:
-  		input_shape = (img_width, img_height, 3)
+  		input_shape = (img_height, img_width, 3)
 
 	data_gen = DataGenerator(train_dir = train_data_dir,
  						  val_dir = val_data_dir,
+ 						  train_size = nb_train,
+ 						  val_size = nb_val,
  						  n_channels = 3,
- 						  batch_size = batch_size,
+ 						  batch_size = batch_sizes,
  						  im_w = img_width,
  						  im_h = img_height,
  						  ds_factor = 2**2
@@ -338,8 +346,8 @@ def train(run_name, start_epoch, stop_epoch):
 	conv_to_rnn_dims = (img_width//(2**2), (img_height // (2**2))*64)
 	rnn = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(cnn)
 	#rnn = Dense(32, activation=cnn_act, name='dense1')(cnn)
-
-	#rnn = Reshape(target_shape=(14,32))(rnn)
+	#print(rnn.shape)
+	#rnn = Reshape(target_shape=(200,32))(rnn)
 	rnn = Bidirectional(LSTM(rnn_size, return_sequences=True,
           kernel_initializer=kernel_init, name='lstm1'))(rnn)
 	rnn = Dropout(0.25)(rnn)
@@ -359,10 +367,10 @@ def train(run_name, start_epoch, stop_epoch):
 	sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
 
 	model = Model(inputs=[Input_data, labels, input_length, label_length], outputs=loss_out)
-	model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+	model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd, metrics=['accuracy'])
 
 	if start_epoch > 0:
-		weights_file = os.path.join(output_dir, os.path.join(run_name, 'weights%02d.h5' % (start_epoch - 1)))
+		weights_file = os.path.join(out_put_dirs, os.path.join(run_name, 'weights%02d.h5' % (start_epoch - 1)))
 		model.load_weights(weights_file)
 
 	test_func = K.function([Input_data], [y_pred])
@@ -371,13 +379,13 @@ def train(run_name, start_epoch, stop_epoch):
 
 	epochs = 10
 	model.fit_generator(generator = data_gen.next_train(),
-						steps_per_epoch = (len(nb_train)//batch_size),
+						steps_per_epoch = (len(nb_train)//batch_sizes - 1),
 						epochs=stop_epoch,
 						validation_data = data_gen.next_val(),
-						validation_steps = (len(nb_val)//batch_size),
+						validation_steps = (len(nb_val)//batch_sizes - 1),
 						callbacks=[viz_cb, data_gen],
 						initial_epoch=start_epoch)
 
-run_name = datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
-train(run_name, 0, 20)
+run_name = "build_1"
+train(run_name,0,40)
 
